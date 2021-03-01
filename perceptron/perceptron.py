b1169f1e47d23fbb7875a05ec3345f334b423387
Flashlock <ajamesbetts1998@gmail.com>
2021-03-01 15:51:37 -0800

    The Perceptron works on my little test. Now to test on real data

---
+++ perceptron/perceptron.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+class Perceptron(object):
+    """
+    The Perceptron Algorithm:
+    
+    Parameters:
+        omega - a vector of length of num axes + 1. each element starts randomized and is updated
+            to converge to the separation
+
+        nu - learning rate. Constant between 0 and 1. Bigger nu's will 
+            converge to the separation faster, but make more mistakes.
+
+    Inputs:
+        x - a point represented by a vector of length of num axes + 1. 
+            x0 = 1.
+            x1 to xn represent the coordinates for this point.
+            there will be an array of x's inputed
+    """
+
+
+    # constant multiplier
+    learning_rate: float
+    # how many times to iterate over dataset
+    iterations: int
+    # initial states are randomized to this seed
+    start_seed: int
+
+    # weights after fitting
+    weights: np.array
+    # misclassifications after fitting
+    errors: list
+
+
+    def __init__(self, learning_rate=.01, iterations=50, start_seed=1):
+        self.learning_rate = learning_rate
+        self.iterations = iterations
+        self.start_seed = start_seed
+
+    def fit(self, training_vectors, target_values):
+        # training vectors is the array of x's
+        # target values are what the x's should be identified as
+        # they must be of the same length
+        assert len(training_vectors) == len(target_values)
+
+        # create a random number generator given the seed
+        rngen = np.random.RandomState(self.start_seed)
+        # get the weights randomly along a bell curve.
+        # loc: distribution center, scale: standard deviation, size: number of samples to return
+        self.weights = rngen.normal(loc=0.0, scale=.01, size=1 + training_vectors.shape[1])
+
+        self.errors = list()
+        # now loop through the vectors, make predictions, log mistakes
+        for iteration in range(0, self.iterations):
+            errors = 0
+            for x, target in zip(training_vectors, target_values):
+                # make the prediction
+                prediction = self.predict(x)
+                # update the weights
+                update = self.learning_rate * (target - prediction)
+                # increment all the weights - update will be 0 if prediction correct
+                self.weights[1:] += update * x
+                self.weights[0] += update
+
+                # count the number of errors
+                errors += 1 if update != 0 else 0
+
+            self.errors.append(errors)
+            # break for perfection
+            if(errors == 0):
+                break
+
+    # determines the net input for some x
+    def net_input(self, x):
+        return np.dot(x, self.weights[1:]) + self.weights[0]
+
+    # makes a prediction for some x
+    def predict(self, x):
+        return np.where(self.net_input(x) >= 0.0, 1, -1)
+
+
+
+perceptron = Perceptron()
+training_vectors=np.array([[1, 2],[3, 4], [5, 6], [-1, 2], [-3, -4], [-5, -6]])
+target_values=np.array([1, 1, 1, -1, -1, -1])
+perceptron.fit(training_vectors, target_values)
+print(perceptron.errors)
